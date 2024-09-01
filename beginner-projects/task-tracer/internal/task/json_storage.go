b168c9ca00cc0067f79175c1a80883d7722982e2
David452 <davidkratky5@gmail.com>
2024-09-01 20:33:15 +0200

    task-tracker json_storage created

--- beginner-projects/task-tracer/internal/task/json_storage.go
+++ beginner-projects/task-tracer/internal/task/json_storage.go
@@ -1,16 +1,48 @@
 package task
 
 import (
-	
+	"encoding/json"
+	"os"
+	"fmt"
 )
 
-const jsonFile = "tasks.json" // The file where tasks are stored
+// The file where tasks are stored
+const jsonFile = "tasks.json"
 
-func saveTasksToJson(tasks []Task) {
-	// TODO
+func saveTaskToJson(task Task) error {
+
+	jsonData, err := json.MarshalIndent(task, "", "  ")
+
+	if err != nil {
+		return fmt.Errorf("error converting structs to JSON format: %w", err)
+	}
+
+	err = os.WriteFile(jsonFile, jsonData, 0644)
+
+	if err != nil {
+		return fmt.Errorf("error writing JSON data to file: %w", err)
+	}
+
+	return nil
 }
 
-func loadTasksFromJson() []Task {
-	// TODO
-	return nil
+func loadTasksFromJson() (*[]Task, error) {
+	
+	// Open the file
+	file, err := os.Open(jsonFile)
+
+	if err != nil {
+		return nil, fmt.Errorf("error opening JSON file: %w", err)
+	}
+	defer file.Close()
+
+	// Decode json
+	var tasks []Task
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&tasks)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding JSON data: %w", err)
+	}
+	
+	return &tasks, nil
 }
