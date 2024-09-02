849beb1348257bd22e6ae9acdcd21f5cdaa693ac
David452 <davidkratky5@gmail.com>
2024-09-02 10:42:14 +0200

    task-tracker pretty print into table

--- beginner-projects/task-tracker/cmd/list.go
+++ beginner-projects/task-tracker/cmd/list.go
@@ -1,11 +1,11 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/David452/task-tracer/internal/task"
 
 	"github.com/urfave/cli/v2"
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/andanhm/go-prettytime"
 )
 
 func ListTasks(cCtx *cli.Context) error {
@@ -16,24 +16,31 @@ func ListTasks(cCtx *cli.Context) error {
 		return err
 	}
 
+	prettyTable := table.NewWriter()
+	prettyTable.SetOutputMirror(cCtx.App.Writer)
+	prettyTable.AppendHeader(table.Row{"ID", "Description", "Status", "Created", "Updated"})
+
+
 	for _, t := range tasks {
 		switch cCtx.Args().First() {
 		case "todo":
 			if t.Status == task.StatusTodo {
-				fmt.Println(t)
+				prettyTable.AppendRow(table.Row{t.ID, t.Description, t.Status, prettytime.Format(t.CreatedAt), prettytime.Format(t.UpdatedAt)})
 			}
 		case "in-progress":
 			if t.Status == task.StatusInProgress {
-				fmt.Println(t)
+				prettyTable.AppendRow(table.Row{t.ID, t.Description, t.Status, prettytime.Format(t.CreatedAt), prettytime.Format(t.UpdatedAt)})
 			}
 		case "done":
 			if t.Status == task.StatusDone {
-				fmt.Println(t)
+				prettyTable.AppendRow(table.Row{t.ID, t.Description, t.Status, prettytime.Format(t.CreatedAt), prettytime.Format(t.UpdatedAt)})
 			}
 		default:
-			fmt.Println(t)
+			prettyTable.AppendRow(table.Row{t.ID, t.Description, t.Status, prettytime.Format(t.CreatedAt), prettytime.Format(t.UpdatedAt)})
 		}
 	}
+
+	prettyTable.Render()
 
 	return nil
 }
--- beginner-projects/task-tracker/go.mod
+++ beginner-projects/task-tracker/go.mod
@@ -2,9 +2,15 @@ module github.com/David452/task-tracer
 
 go 1.22.2
 
+require github.com/urfave/cli/v2 v2.27.4 // direct
+
 require (
+	github.com/andanhm/go-prettytime v1.1.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
+	github.com/jedib0t/go-pretty/v6 v6.5.9 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/urfave/cli/v2 v2.27.4 // direct
 	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
+	golang.org/x/sys v0.17.0 // indirect
 )
