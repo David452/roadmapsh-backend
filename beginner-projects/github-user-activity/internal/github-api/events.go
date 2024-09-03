9498540e537dc48ab22a7446f9c50534ac5151e1
David452 <davidkratky5@gmail.com>
2024-09-03 12:15:51 +0200

    github-user-activity fetch events

--- beginner-projects/github-user-activity/internal/github-api/events.go
+++ beginner-projects/github-user-activity/internal/github-api/events.go
@@ -1,6 +1,53 @@
 package githubapi
 
-func GetEvents() (*Events, error) {
-	// TODO
-	return &Events{}, nil
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+)
+
+func (c *Client) GetEvents(user string) (*Activity, error) {
+	
+	url := fmt.Sprintf("%s/users/%s/events", c.baseURL, user)
+
+	req, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		return &Activity{}, fmt.Errorf("error creating request: %w", err)
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return &Activity{}, fmt.Errorf("error sending request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 304 {
+
+		return &Activity{}, fmt.Errorf("not modified")
+	}
+
+	if resp.StatusCode == 403 {
+
+		return &Activity{}, fmt.Errorf("forbidden")
+	}
+
+	if resp.StatusCode == 503 {
+
+		return &Activity{}, fmt.Errorf("service unavailable")
+	}
+
+	if resp.StatusCode != 200 {
+		return &Activity{}, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+
+	var activity Activity
+	err = json.NewDecoder(resp.Body).Decode(&activity)
+	if err != nil {
+		return &Activity{}, fmt.Errorf("error decoding JSON response: %w", err)
+	}
+
+
+	return &activity, nil
 }
