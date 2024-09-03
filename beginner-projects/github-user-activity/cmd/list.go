682f756a8fa1c4f113efb905205acc2aedca25c5
David452 <davidkratky5@gmail.com>
2024-09-03 12:35:26 +0200

    github-user-activity repaired json marshal and prettier printing

--- beginner-projects/github-user-activity/cmd/list.go
+++ beginner-projects/github-user-activity/cmd/list.go
@@ -8,13 +8,40 @@ import (
 
 func List(user string,client *githubapi.Client) error {
 
-	activity, err := client.GetEvents(user)
+	activities, err := client.GetEvents(user)
 
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(activity)
+	if len(activities) == 0 {
+
+		fmt.Println("No events found")
+		return nil
+	}
+
+	fmt.Printf("Events for %s\n", user)
+
+	for _, activity := range activities {
+		var action string
+		switch activity.Type {
+		case "PushEvent":
+			commitCount := len(activity.Payload.Commits)
+			action = fmt.Sprintf("Pushed %d commit(s) to %s", commitCount, activity.Repo.Name)
+		case "IssuesEvent":
+			action = fmt.Sprintf("%s an issue in %s", activity.Payload.Action, activity.Repo.Name)
+		case "WatchEvent":
+			action = fmt.Sprintf("Starred %s", activity.Repo.Name)
+		case "ForkEvent":
+			action = fmt.Sprintf("Forked %s", activity.Repo.Name)
+		case "CreateEvent":
+			action = fmt.Sprintf("Created %s in %s", activity.Payload.RefType, activity.Repo.Name)
+		default:
+			action = fmt.Sprintf("%s in %s", activity.Type, activity.Repo.Name)
+		}
+
+		fmt.Println("- ", action)
+	}
 	
 	return nil
 }
--- beginner-projects/github-user-activity/internal/githubapi/events.go
+++ beginner-projects/github-user-activity/internal/githubapi/events.go
@@ -6,48 +6,48 @@ import (
 	"encoding/json"
 )
 
-func (c *Client) GetEvents(user string) (*Activity, error) {
+func (c *Client) GetEvents(user string) ([]Activity, error) {
 	
 	url := fmt.Sprintf("%s/users/%s/events", c.baseURL, user)
 
 	req, err := http.NewRequest("GET", url, nil)
 
 	if err != nil {
-		return &Activity{}, fmt.Errorf("error creating request: %w", err)
+		return nil, fmt.Errorf("error creating request: %w", err)
 	}
 
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
-		return &Activity{}, fmt.Errorf("error sending request: %w", err)
+		return nil, fmt.Errorf("error sending request: %w", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode == 304 {
 
-		return &Activity{}, fmt.Errorf("not modified")
+		return nil, fmt.Errorf("not modified")
 	}
 
 	if resp.StatusCode == 403 {
 
-		return &Activity{}, fmt.Errorf("forbidden")
+		return nil, fmt.Errorf("forbidden")
 	}
 
 	if resp.StatusCode == 503 {
 
-		return &Activity{}, fmt.Errorf("service unavailable")
+		return nil, fmt.Errorf("service unavailable")
 	}
 
 	if resp.StatusCode != 200 {
-		return &Activity{}, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
 	}
 
 
-	var activity Activity
-	err = json.NewDecoder(resp.Body).Decode(&activity)
+	var activities []Activity
+	err = json.NewDecoder(resp.Body).Decode(&activities)
 	if err != nil {
-		return &Activity{}, fmt.Errorf("error decoding JSON response: %w", err)
+		return nil, fmt.Errorf("error decoding JSON response: %w", err)
 	}
 
 
-	return &activity, nil
+	return activities, nil
 }
