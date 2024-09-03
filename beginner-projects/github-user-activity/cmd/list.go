1a0b6224111a735ba75c978543fc2aef077a3b37
David452 <davidkratky5@gmail.com>
2024-09-03 12:27:23 +0200

    github-user-activityj cli

--- beginner-projects/github-user-activity/cmd/list.go
+++ beginner-projects/github-user-activity/cmd/list.go
@@ -1,5 +1,20 @@
 package cmd
 
-func List() {
-	// TODO
+import (
+	"fmt"
+
+	"github.com/David452/github-user-activity/internal/githubapi"
+)
+
+func List(user string,client *githubapi.Client) error {
+
+	activity, err := client.GetEvents(user)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(activity)
+	
+	return nil
 }
--- beginner-projects/github-user-activity/main.go
+++ beginner-projects/github-user-activity/main.go
@@ -1,7 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"log"
 
+	"github.com/David452/github-user-activity/internal/githubapi"
+	"github.com/David452/github-user-activity/cmd"
+
+	"github.com/urfave/cli/v2"
+)
 
 func main() {
-	// TODO
+
+	client := githubapi.NewClient("https://api.github.com")
+	
+	app := &cli.App{
+		Name: "github-user-activity",
+		Usage: "Get user activity",
+		Action: func (cCtx *cli.Context) error {
+			if cCtx.NArg() == 0 {
+				return fmt.Errorf("missing user name")
+			}
+
+			return cmd.List(cCtx.Args().First(), client)
+		},
+		ArgsUsage: "[user]",
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
