7847fb6926e73b22c131c188461a8297724c3927
David452 <davidkratky5@gmail.com>
2024-09-04 11:36:04 +0200

    unit-converter serve index.html

--- beginner-projects/unit-converter/main.go
+++ beginner-projects/unit-converter/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"net/http"
+	"fmt"
+)
+
+const (
+	PORT = ":8080"
+)
+
 func main() {
-	// TODO
+
+	fmt.Printf("Listening on port %s\n", PORT)
+
+	fs := http.FileServer(http.Dir("./web/static"))
+
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./web/templates/index.html")
+	})
+
+	http.ListenAndServe(PORT, nil)
 }
