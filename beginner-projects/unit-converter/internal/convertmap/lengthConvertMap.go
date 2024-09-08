f134f89588c3aa5a8f99c2c3c65702e72822694e
David452 <davidkratky5@gmail.com>
2024-09-08 21:13:44 +0200

    unit-converter renamed maps

---
+++ beginner-projects/unit-converter/internal/convertmap/lengthConvertMap.go
@@ -0,0 +1,13 @@
+package convertmap
+
+var LengthConversionMap = map[string]float64{
+	"millimeter": 1,
+	"centimeter": 10,
+	"decimeter": 100,
+	"meter": 1000,
+	"kilometer": 1e6,
+	"mile": 1609344,
+	"yard": 914,
+	"foot": 304.8,
+	"inch": 25.4,
+}
